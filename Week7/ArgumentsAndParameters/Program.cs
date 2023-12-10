251e9764974ccc2855ee59b640627f4cf73a07eb
Uku <149318155+Ukutktk@users.noreply.github.com>
2023-12-10 21:56:29 +0200

    Add files via upload

---
+++ Week7/ArgumentsAndParameters/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+// Kirjuta etteantud sõna suurte tahtedega 10 korda
+
+Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+PrintAnyWord(userInput); //userInput on argument
+
+static void PrintAnyWord(string anyString) //anystring on parameeter
+{
+    anyString = anyString.ToUpper();
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
---
+++ Week7/Week7Functions/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+
+//Console.WriteLine("Hello, World!"); Tuled või lähed
+
+using System.Threading.Channels;
+
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoice = Console.ReadLine();
+
+if(userChoice == "in")
+{
+    PrintHello();   
+}
+else
+{
+    PrintGoodBye();     
+}
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, alligator.");
+}
+  
